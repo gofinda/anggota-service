594bb40d207f5d28893284c1bc1010c7d4d274aa
gofinda <113493633+gofinda@users.noreply.github.com>
2023-01-03 22:29:24 +0700

    Initial commit

---
+++ src/main/java/com/gofinda/anggota/service/repository/AnggotaRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.gofinda.anggota.service.repository;
+
+import com.gofinda.anggota.service.entity.Anggota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author User
+ */
+@Repository
+public interface AnggotaRepository extends JpaRepository<Anggota, Long> {
+    
+    public Anggota findByAnggotaId(Long anggotaId);
+}
